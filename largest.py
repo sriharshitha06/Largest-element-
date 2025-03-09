23c2359118306634ebc965985e0c288fc1398170
MANTHENA SRI HARSHITHA <manthenasiri23@gmail.com>
2025-03-09 22:04:36 +0530

    Update largest.py

--- largest.py
+++ largest.py
@@ -3,7 +3,7 @@ def sortArr(arr: List[int]) -> int:
     arr.sort()
     return arr[-1]
 if __name__ == "__main__":
-    arr1 = [2, 5, 1, 3, 0]
-    arr2 = [8, 10, 5, 7, 9]
-    print("The Largest element in the array is:", sortArr(arr1))
-    print("The Largest element in the array is:", sortArr(arr2))
+    n = int(input())  
+    arr = list(map(int, input().split())) 
+    print(sortArr(arr))
+
